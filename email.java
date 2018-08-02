3d62db2bf34e19a250aad982fc6b740708ddcbda
tasneemp <36156040+tasneemp@users.noreply.github.com>
2018-08-02 08:33:52 +0530

    Update email.java

--- email.java
+++ email.java
@@ -6,17 +6,18 @@ public class email {
 
 	public static void main(String[] args) {
 		String email;
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the email address+");
-        email=sc.nextLine();
-		
-		
-		int i1=email.indexOf('@');
-		int i2=email.indexOf('.');
-		
-		if(!(email.contains("@")&&email.contains("."))||email.substring(0,i1).length()<4||email.substring(i1+1, i2).length()<3||email.substring(i2+1, email.length()).length()<2)
+		email = sc.nextLine();
+
+		int i1 = (email.indexOf('@')==email.lastIndexOf('@'))?email.indexOf('@'):-1;
+		int i2 = (email.indexOf('.')==email.lastIndexOf('.'))?email.indexOf('.'):-1;
+
+		if (i1<0||i2<0||!(email.contains("@") && email.contains(".")) || email.substring(0, i1).length() < 4
+				|| email.substring(i1 + 1, i2).length() < 3 || email.substring(i2 + 1, email.length()).length() < 2)
 			System.out.println("rejected");
-		else System.out.println("accepted");	
+		else
+			System.out.println("accepted");
 	}
 
 }
